54fca5ba970713bdd21b0f22e0121c25b5a4088e
jovro <jorge020499@gmail.com>
2022-11-12 21:34:50 -0500

    Inicia el manejo de variables de entorno

---
+++ env.js
@@ -0,0 +1,2 @@
+/* eslint-disable prettier/prettier */
+export const API_URL = import.meta.env.VITE_API_URL;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { API_URL } from "../env";
+
 function App() {
-  return <h1>Hola mundo</h1>;
+  return (
+    <div>
+      <h1>Hola {API_URL}</h1>
+    </div>
+  );
 }
 
 export default App;
